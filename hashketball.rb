e9c884d68c38daaa7268bc46ff1389598af14fba
Kathleen Brennan <brennank14@gmail.com>
2020-06-14 19:05:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -133,7 +133,6 @@ def num_points_scored(player)
   game_hash.each do |keys, values|
     values.each do |stat_name|
       if player == game_hash[keys][:players][:player_name]
-        binding.pry
         [stat_name][:points]
       end
     end
